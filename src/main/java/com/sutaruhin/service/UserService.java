9513ccee8185839d55a60644629a17a26178d011
shout <shout@192.168.11.4>
2023-11-06 21:29:21 +0900

    ユーザ一覧表示機能を実装

---
+++ src/main/java/com/sutaruhin/service/UserService.java
@@ -0,0 +1,18 @@
+package com.sutaruhin.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sutaruhin.entity.User;
+import com.sutaruhin.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> getUserList(){
+		return userRepository.findAll();
+	}
+}
